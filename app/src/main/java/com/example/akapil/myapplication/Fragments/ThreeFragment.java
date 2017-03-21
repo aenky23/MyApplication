2ce496fc98dd6b2740d173986138ce3fbd57a8b6
akapil <aenky23@gmail.com>
2017-03-21 14:47:11 +0530

    web View

--- app/src/main/java/com/example/akapil/myapplication/Fragments/ThreeFragment.java
+++ app/src/main/java/com/example/akapil/myapplication/Fragments/ThreeFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 import com.example.akapil.myapplication.R;
 
@@ -15,6 +16,8 @@ import com.example.akapil.myapplication.R;
  */
 
 public class ThreeFragment extends Fragment {
+
+    private WebView webView;
 
     public ThreeFragment() {
 
@@ -28,6 +31,9 @@ public class ThreeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_three, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_three, container, false);
+        webView = (WebView) rootView.findViewById(R.id.web_container);
+        webView.loadUrl("https://www.google.co.in/");
+        return rootView;
     }
 }
