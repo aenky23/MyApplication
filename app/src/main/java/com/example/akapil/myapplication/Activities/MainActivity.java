58f47e3bb5d6775d8ccaaea7632aa053ea64c9e0
akapil <aenky23@gmail.com>
2017-03-28 09:29:46 +0530

    no action bar

--- app/src/main/java/com/example/akapil/myapplication/Activities/MainActivity.java
+++ app/src/main/java/com/example/akapil/myapplication/Activities/MainActivity.java
@@ -1,32 +1,21 @@
 package com.example.akapil.myapplication.Activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.widget.ListView;
 
 import com.example.akapil.myapplication.Adapters.PagerAdapter;
-import com.example.akapil.myapplication.Models.Book;
 import com.example.akapil.myapplication.R;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView itemsList;
-    private ArrayList<Book> dataList = null;
-    private FloatingActionButton viewCart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
@@ -60,32 +49,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-//        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-//        getSupportActionBar().setCustomView(R.layout.custom_actionbar);
-//        fetchData();
-//        initUI();
     }
-
-    /*private void initUI() {
-        itemsList = (ListView) findViewById(R.id.lv);
-        viewCart = (FloatingActionButton) findViewById(R.id.fab);
-
-        itemsList.setAdapter(new CustomAdapter(getApplicationContext(), dataList));
-        viewCart.setOnClickListener(this);
-    }
-
-    private void fetchData() {
-        dataList = DBHelper.getInstance(getApplicationContext()).getBooks();
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.fab:
-                Intent i = new Intent(getApplicationContext(), CartActivity.class);
-                startActivity(i);
-                break;
-        }
-    }*/
 }
